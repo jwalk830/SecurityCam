ca72ea90de5b8a35342d80c9c4e8cc2a2f381181
jwalk <justin.t.walk@gmail.com>
2016-12-30 16:25:04 -0700

    working on things, need to push to pull on pi

--- Security/Security.py
+++ Security/Security.py
@@ -5,16 +5,16 @@ from time import sleep
 class Security:
     def __init__(self, motion_pin = 17, button_pin = 18, sleep_in_seconds = 5):
         #Variables
-        self.pir = MotionSensor(motion_pin)
-        self.button = Button(button_pin)
+        pir = MotionSensor(motion_pin)
+        button = Button(button_pin)
         self.is_armed = False
         self.is_recording = False
         self.sleep_in_seconds = sleep_in_seconds
         
         #Event Handlers
-        self.pir.when_motion = self.start_recording
-        self.pir.when_no_motion = self.stop_recording
-        self.button.when_pressed = self.toggle_armed
+        pir.when_motion = self.start_recording
+        pir.when_no_motion = self.stop_recording
+        button.when_pressed = self.toggle_armed
 
     def start_recording():
         if self.is_armed:
