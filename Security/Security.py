bbab4c1be2cb6673a4898efc30c02e1663887580
jwalk <justin.t.walk@gmail.com>
2016-12-30 16:27:14 -0700

    fixed order

--- Security/Security.py
+++ Security/Security.py
@@ -3,19 +3,6 @@ from time import sleep
 
 
 class Security:
-    def __init__(self, motion_pin = 17, button_pin = 18, sleep_in_seconds = 5):
-        #Variables
-        pir = MotionSensor(motion_pin)
-        button = Button(button_pin)
-        self.is_armed = False
-        self.is_recording = False
-        self.sleep_in_seconds = sleep_in_seconds
-        
-        #Event Handlers
-        pir.when_motion = self.start_recording
-        pir.when_no_motion = self.stop_recording
-        button.when_pressed = self.toggle_armed
-
     def start_recording():
         if self.is_armed:
             self.is_recording = True
@@ -35,3 +22,15 @@ class Security:
             print("Entered not function")
             self.stop_recording()
 
+    def __init__(self, motion_pin = 17, button_pin = 18, sleep_in_seconds = 5):
+        #Variables
+        pir = MotionSensor(motion_pin)
+        button = Button(button_pin)
+        self.is_armed = False
+        self.is_recording = False
+        self.sleep_in_seconds = sleep_in_seconds
+        
+        #Event Handlers
+        pir.when_motion = self.start_recording
+        pir.when_no_motion = self.stop_recording
+        button.when_pressed = self.toggle_armed
