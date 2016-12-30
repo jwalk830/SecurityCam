c404a72b35c2b5b3d4aece7bf45aa88cc83c9c0a
jwalk <justin.t.walk@gmail.com>
2016-12-30 16:34:55 -0700

    trying some tweaks

--- Security/Security.py
+++ Security/Security.py
@@ -24,13 +24,13 @@ class Security:
 
     def __init__(self, motion_pin = 17, button_pin = 18, sleep_in_seconds = 5):
         #Variables
-        pir = MotionSensor(motion_pin)
-        button = Button(button_pin)
+        self.pir = MotionSensor(motion_pin)
+        self.button = Button(button_pin)
         self.is_armed = False
         self.is_recording = False
         self.sleep_in_seconds = sleep_in_seconds
         
         #Event Handlers
-        pir.when_motion = self.start_recording
-        pir.when_no_motion = self.stop_recording
-        button.when_pressed = self.toggle_armed
+        # pir.when_motion = self.start_recording
+        # pir.when_no_motion = self.stop_recording
+        # button.when_pressed = self.toggle_armed
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from Security import Security
+from Security.Security import Security
 from signal import pause
 
-Security.Security(17,18,3)
+security = Security(17,18,3)
+security.pir.when_motion = security.start_recording
+security.pir.when_no_motion = security.stop_recording
+security.button.when_pressed = security.toggle_armed
 pause()
