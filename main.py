a09986dd1058621b582caaeed95cc223e53b9d81
jwalk <justin.t.walk@gmail.com>
2016-12-31 00:04:55 -0700

    fixed call

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Security.Security import Security
 from Temperature.Temperature import Temperature
 
 
-temp = Temperature('28-0316563a9dff', False)
+temp = Temperature(False,'28-0316563a9dff')
 temp.get_temperature()
 security = Security(17, 18, 3)
 security.secure_up()
