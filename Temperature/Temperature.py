89255a8b3065a44cf7f090a1a3f5e3ad855626e6
jwalk <justin.t.walk@gmail.com>
2016-12-30 23:25:00 -0700

    modified temp class

--- Temperature/Temperature.py
+++ Temperature/Temperature.py
@@ -12,15 +12,9 @@ class Temperature:
     def __init__(self, is_celsius, ds18b20='28-0316563a9dff'):
         self.ds18b20 = ds18b20
         self.is_celsius = is_celsius
-        self.setup()
+        self.set_temperature()
 
-    def setup(self):
-        for i in os.listdir('/sys/bus/w1/devices'):
-            if i != 'w1_bus_master1':
-                self.ds18b20 = i
-
-
-    def read(self):
+    def set_temperature(self):
         location = '/sys/bus/w1/devices/' + self.ds18b20 + '/w1_slave'
         tfile = open(location)
         text = tfile.read()
@@ -32,14 +26,11 @@ class Temperature:
         if not self.is_celsius:
             temperature = temperature * 9 / 5 + 32
         self.temperature = temperature
-        return temperature
-    
-    def destroy(self):
-        pass
 
     def get_temperature(self):
         if self.is_celsius:
-            print "Current temperature : %0.3f C" % self.read()
+            print "Current temperature : %0.3f C" % self.temperature
         else:
-            print "Current temperature : %0.3f F" % self.read()
+            print "Current temperature : %0.3f F" % self.temperature
         return self.temperature
+
