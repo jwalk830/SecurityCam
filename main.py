f818128ec26463b97ad430a097fe5da9e9c5fb23
jwalk <justin.t.walk@gmail.com>
2017-01-05 12:13:37 -0700

    Removed temperature and updated security to use pwm.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from Security.Security import Security
-from Temperature.Temperature import Temperature
 
-
-temp = Temperature(False,'28-0316563a9dff')
-temp.get_temperature()
 security = Security(17, 18, 3)
 security.secure_up()
